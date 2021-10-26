18b48bdf552f44de2117a81d37626af17f397c4a
bandarishivani <89641388+bandarishivani@users.noreply.github.com>
2021-10-26 11:46:08 +0530

    Add files via upload

---
+++ commandline-demo/4-PermutationApp/App.java
@@ -0,0 +1,13 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5, r=3;
+
+        int p=Permutation.calculate(n,r);
+
+        System.out.printf("%d P %d is %d\n", n,r,p);
+	}
+
+}
