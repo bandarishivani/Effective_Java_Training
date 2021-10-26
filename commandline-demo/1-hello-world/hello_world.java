3e3b9cd4fc2f8aa8d602b255c12e78810bdd584d
bandarishivani <89641388+bandarishivani@users.noreply.github.com>
2021-10-26 11:31:34 +0530

    Add files via upload

---
+++ commandline-demo/1-hello-world/hello_world.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author lenovo
+ *
+ */
+public class hello_world {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
